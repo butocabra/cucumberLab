11e8e7faeb7d9f2ba746937bab3d0f7d2b044c3f
user <user@ubuntu1010desktop.(none)>
2011-03-20 05:53:33 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamelib::Application.config.secret_token = 'de6251d4a88083d7133b7c75125eaf1cd7dc45671c05a6c6016c8a1d662fa3987e9b882ff3cb28f0ef90f8a215328ee34295927415958c6f3eef7edceb232092'
