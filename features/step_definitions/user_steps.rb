b13378054cadfdf85f914f914593b099baecd01b
Eric Burley <eric@eburley.com>
2011-04-09 16:49:26 +0200

    working on getting features green again.

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,8 +1,19 @@
 Given /^that I am not logged in$/ do
-  pending # express the regexp above with the code you wish you had
 end
 
 Then /^I should be logged in$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^a user with the email "([^"]*)" will exist$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^that I am logged in$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should not be logged in$/ do
   pending # express the regexp above with the code you wish you had
 end
 
